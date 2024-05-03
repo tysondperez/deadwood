8bfe0c1bceed8ec521ae0c82c7586237d6311ea1
Ha-My Anklam <ha-my@Something.Doggy>
2024-05-03 14:41:26 -0700

    add rank tracker

---
+++ RankTracker.java
@@ -0,0 +1,23 @@
+
+public class rankTracker {
+	
+	//object type player
+
+	public void choosePayment()
+	{
+		
+	}
+	
+	public void chooseRank()
+	{
+		//int rank1 = 
+	}
+	
+	public void checkPayment()
+	{
+		//if payment > rankChosen, turn die
+		//if payment < rankChosen
+	}
+	
+
+}
