ca135b610a611fa6b9e1d27fa994611d77f5e052
anklams <anklams@wwu.edu>
2024-05-13 23:14:26 -0700

    add method called viewScene

--- Role.java
+++ Role.java
@@ -3,4 +3,11 @@ public class Role {
 
     private int level;
     private boolean isTaken;
+    
+    public void viewScene()
+    {
+    	//at location, view the scene, get available roles and rank requirement from xml file
+    	//find empty roles
+    	
+    }
 }
