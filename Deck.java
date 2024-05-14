f9d9aaa5b40fccdb840e445abe14715ee207ce84
anklams <anklams@wwu.edu>
2024-05-13 23:07:55 -0700

    subtract from total available cards, need to read from xml file

--- Deck.java
+++ Deck.java
@@ -2,9 +2,17 @@
 public class Deck{
 	
 	private int totalCards;
-	private int cardsLeft;
+//	private int cardsLeft; oops, redundant
 
 	public void getCard(){
+		
+		//will get cards from the xml file
 	
 	}	
+	
+	public void dealCards()
+	{
+		//for each location, deal a card, need xml
+		totalCards = totalCards - 1;
+	}
 }
