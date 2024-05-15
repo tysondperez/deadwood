77559c4a667bd64e51c933681b69a4fc9fa8fd5b
anklams <anklams@wwu.edu>
2024-05-14 20:19:41 -0700

    Replace Off_Card.java

--- Off_Card.java
+++ Off_Card.java
@@ -1,6 +1,13 @@
 public class Off_Card extends Role {
     
-    public void getRewards(){
+    public void getRewards(boolean success){
+    	
+    	if (success) {
+    		//add one dollar and one credit
+    	}
+    	else {
+    		//add one dollar
+    	}
         
     }
 }
